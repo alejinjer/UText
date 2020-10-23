2006fd908a1f6f35a357ee39352bdc4901189f68
rogalick <46848055+rogalick@users.noreply.github.com>
2020-10-23 16:02:01 +0300

    add Directory Tree

--- mainwindow.h
+++ mainwindow.h
@@ -10,6 +10,9 @@
 #include<QPrinter>
 #include<QPrintDialog>
 #include <QTextStream>
+#include <QFileSystemModel>
+#include<QDebug>
+#include<QShortcut>
 
 
 namespace Ui {
@@ -31,9 +34,28 @@ private slots:
 
     void on_actionSave_triggered();
 
+    void on_treeView_doubleClicked(const QModelIndex &index);
+
+    void on_actionCopy_triggered();
+
+    void on_actionPaste_triggered();
+
+    void on_actionCut_triggered();
+
+    void on_actionUndo_triggered();
+
+    void on_actionRedo_triggered();
+
+    void slotShortcutCtrlC();
+
 private:
     Ui::MainWindow *ui;
+
     QString currentFile = "";
+
+    QFileSystemModel*model;
+
+    QShortcut       *CtrlC;
 };
 
 #endif // MAINWINDOW_H
